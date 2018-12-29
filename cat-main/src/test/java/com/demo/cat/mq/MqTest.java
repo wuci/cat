436a9680e5a0e3f632df588874e022f3a32f163b
wuci <wuci@zhuojianchina.com>
2018-12-29 15:45:56 +0800

    first commt

---
+++ cat-main/src/test/java/com/demo/cat/mq/MqTest.java
@@ -0,0 +1,22 @@
+package com.demo.cat.mq;
+
+import com.demo.cat.CatMainApplicationTests;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * /**
+ *
+ * @author: wuci
+ * @date: 2018/7/19
+ * @copyright:zjkj
+ * @since : v 1.0
+ */
+public class MqTest extends CatMainApplicationTests {
+    private static final Logger LOG = LoggerFactory.getLogger(MqTest.class);
+
+    @Test
+    public void mqTest() {
+    }
+}
